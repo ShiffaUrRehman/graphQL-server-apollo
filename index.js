510c7f17b0a5b6d07a02bc305d05ca6d168655f0
Shiffa Ur Rehman <22100167@lums.edu.pk>
2023-11-22 16:42:40 +0500

    resolvers

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-// types
+// db
+import db from "./_db.js";
 
-import { typeDefs } from "./schema";
+// types
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 //server setup
 const server = new ApolloServer({
   // typeDefs -- definitions of types of data
   typeDefs,
   // resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
